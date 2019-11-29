871df29cabbf824c40f60b742379f0ce1c6b1a5a
Eliana Nino <enino@H5751.ad.hugeinc.com>
2019-11-29 11:58:29 -0500

    nuevoelement

--- src/helpers.js
+++ src/helpers.js
@@ -20,6 +20,8 @@ export const pickSome = (arr, amount) => {
         const deleted = arr.splice(idx, 1);
         picked.push(deleted[0]);
         document.write("nuevo general");
+        document.write("nuevo general");
+        document.write("nuevo general");
     }
 
     return picked;
